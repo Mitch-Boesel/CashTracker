08943ade891444fe710e1094fcdb114e04a16a3c
Mitch Boesel <mitchall.boesel@wsu.edu>
2020-07-08 15:53:47 -0700

    initial api service commit

---
+++ CashTrackerApiService/Models/PostgresConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashTrackerApiService
+{
+    public class PostgresConnection
+    {
+        public string Host { get; set; }
+        public string Username { get; set; }
+
+        public string Database { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConnectionString { get; set; }
+
+    }
+
+
+}
---
+++ CashTrackerApiService/Utils.cs
@@ -0,0 +1,17 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashTrackerApiService
+{
+    public class Utils
+    {
+        public static string BuildPsqlConnectionString(PostgresConnection pg)
+        {
+            var connectionString = $"Host={pg.Host};Username={pg.Username};Database={pg.Database};password={pg.Password}";
+            return connectionString;
+        }
+    }
+}
